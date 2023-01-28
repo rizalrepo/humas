30a1f7b8f83ede77d581e48ff64236d9e5822770
rizalrepo <rizalyusei@gmail.com>
2023-01-28 19:45:30 +0800

    add db

---
+++ db/db_humas.sql
@@ -0,0 +1,343 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2023 at 12:45 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_humas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspirasi`
+--
+
+CREATE TABLE `aspirasi` (
+  `id_aspirasi` int(11) NOT NULL,
+  `nm_lengkap` varchar(50) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` varchar(10) NOT NULL,
+  `pesan` text NOT NULL,
+  `status` char(1) NOT NULL,
+  `tindakan` varchar(100) DEFAULT NULL,
+  `id_unit_kerja` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aspirasi`
+--
+
+INSERT INTO `aspirasi` (`id_aspirasi`, `nm_lengkap`, `no_hp`, `tanggal`, `jam`, `pesan`, `status`, `tindakan`, `id_unit_kerja`) VALUES
+(1, 'John Mayer', '+6285248176794', '2023-01-06', '15:31', 'test aspirasi', '2', 'oke', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disposisi`
+--
+
+CREATE TABLE `disposisi` (
+  `id_disposisi` int(11) NOT NULL,
+  `id_surat_masuk` int(11) NOT NULL,
+  `id_unit_kerja` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `disposisi`
+--
+
+INSERT INTO `disposisi` (`id_disposisi`, `id_surat_masuk`, `id_unit_kerja`) VALUES
+(9, 3, 4),
+(10, 3, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_kegiatan`
+--
+
+CREATE TABLE `jenis_kegiatan` (
+  `id_jenis_kegiatan` int(11) NOT NULL,
+  `nm_jenis_kegiatan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jenis_kegiatan`
+--
+
+INSERT INTO `jenis_kegiatan` (`id_jenis_kegiatan`, `nm_jenis_kegiatan`) VALUES
+(2, 'Publikasi'),
+(3, 'Protokol dan Aspirasi'),
+(4, 'Pengajuan dan Pengembangan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_surat`
+--
+
+CREATE TABLE `jenis_surat` (
+  `id_jenis_surat` int(11) NOT NULL,
+  `nm_jenis_surat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jenis_surat`
+--
+
+INSERT INTO `jenis_surat` (`id_jenis_surat`, `nm_jenis_surat`) VALUES
+(2, 'Surat Keputusan'),
+(3, 'Surat Edaran'),
+(5, 'Surat Permohonan'),
+(6, 'Surat Tugas'),
+(7, 'Surat Dinas'),
+(8, 'Surat Perintah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id_kegiatan` int(11) NOT NULL,
+  `nm_kegiatan` varchar(100) NOT NULL,
+  `id_jenis_kegiatan` int(11) NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_selesai` date NOT NULL,
+  `tempat` varchar(100) NOT NULL,
+  `ket` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kegiatan`
+--
+
+INSERT INTO `kegiatan` (`id_kegiatan`, `nm_kegiatan`, `id_jenis_kegiatan`, `tgl_mulai`, `tgl_selesai`, `tempat`, `ket`) VALUES
+(1, 'Test Kegiatan2', 3, '2023-01-16', '2023-01-17', 'Test Tempat2', 'test ket2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaduan`
+--
+
+CREATE TABLE `pengaduan` (
+  `id_pengaduan` int(11) NOT NULL,
+  `nm_lengkap` varchar(50) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` varchar(10) NOT NULL,
+  `pesan` text NOT NULL,
+  `status` char(1) NOT NULL,
+  `tindakan` varchar(100) DEFAULT NULL,
+  `id_unit_kerja` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `nm_lengkap`, `no_hp`, `tanggal`, `jam`, `pesan`, `status`, `tindakan`, `id_unit_kerja`) VALUES
+(1, 'John Lennon', '+6287813567789', '2023-01-06', '15:32', 'test pengaduan', '2', 'siapp', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `surat_masuk`
+--
+
+CREATE TABLE `surat_masuk` (
+  `id_surat_masuk` int(11) NOT NULL,
+  `no_surat` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `perihal` text NOT NULL,
+  `pengirim` varchar(50) NOT NULL,
+  `id_jenis_surat` int(11) NOT NULL,
+  `tgl_terima` date NOT NULL,
+  `bukti` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `surat_masuk`
+--
+
+INSERT INTO `surat_masuk` (`id_surat_masuk`, `no_surat`, `tgl_surat`, `perihal`, `pengirim`, `id_jenis_surat`, `tgl_terima`, `bukti`) VALUES
+(3, '1223232', '2022-12-28', 'Undangan menghadiri acara ', 'Ormas', 5, '2023-01-05', '22576.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_kerja`
+--
+
+CREATE TABLE `unit_kerja` (
+  `id_unit_kerja` int(11) NOT NULL,
+  `nm_unit_kerja` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_kerja`
+--
+
+INSERT INTO `unit_kerja` (`id_unit_kerja`, `nm_unit_kerja`) VALUES
+(2, 'Humas'),
+(3, 'Tata Usaha'),
+(4, 'Kepegawaian'),
+(5, 'Keprotokolan'),
+(6, 'Perencaan dan Keuangan'),
+(7, 'Persidangan'),
+(8, 'Perundang-Undangan'),
+(9, 'Layanan Aspirasi dan Pengaduan'),
+(10, 'Kelengkapan Dewan dan Rapat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nm_user` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `level` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nm_user`, `username`, `password`, `level`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1'),
+(2, 'Kabag Humas', 'humas', '94da7343e47802652a24444298012b8c', '2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD PRIMARY KEY (`id_aspirasi`);
+
+--
+-- Indexes for table `disposisi`
+--
+ALTER TABLE `disposisi`
+  ADD PRIMARY KEY (`id_disposisi`);
+
+--
+-- Indexes for table `jenis_kegiatan`
+--
+ALTER TABLE `jenis_kegiatan`
+  ADD PRIMARY KEY (`id_jenis_kegiatan`);
+
+--
+-- Indexes for table `jenis_surat`
+--
+ALTER TABLE `jenis_surat`
+  ADD PRIMARY KEY (`id_jenis_surat`);
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`id_kegiatan`);
+
+--
+-- Indexes for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD PRIMARY KEY (`id_pengaduan`);
+
+--
+-- Indexes for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  ADD PRIMARY KEY (`id_surat_masuk`);
+
+--
+-- Indexes for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  ADD PRIMARY KEY (`id_unit_kerja`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  MODIFY `id_aspirasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `disposisi`
+--
+ALTER TABLE `disposisi`
+  MODIFY `id_disposisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `jenis_kegiatan`
+--
+ALTER TABLE `jenis_kegiatan`
+  MODIFY `id_jenis_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `jenis_surat`
+--
+ALTER TABLE `jenis_surat`
+  MODIFY `id_jenis_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  MODIFY `id_surat_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `unit_kerja`
+--
+ALTER TABLE `unit_kerja`
+  MODIFY `id_unit_kerja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
